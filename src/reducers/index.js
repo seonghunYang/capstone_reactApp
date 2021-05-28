49158e724e28dfdac08a26081f51a6a89033186b
seonghunYang <didtjdgns852@naver.com>
2021-05-28 21:56:38 +0900

    환경 설정

---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+import produce from "immer";
+
+const initialState= {
+}
+
+const reducer = produce((state, action) => {
+  switch(action.type) {
+    default:
+      break;
+  }
+}, initialState);
+
+export default reducer;
