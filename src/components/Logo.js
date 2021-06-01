2d3bcc4b2882f365f2a0b7927ccf23ada1390ebd
JiWon <jiohning00@hanmail.net>
2021-06-01 16:16:25 +0900

    logo 변경

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,11 +1,11 @@
 import { Link, Image } from "@chakra-ui/react";
-import logo from "../images/logo.png";
+import logo from "../images/logo2.png";
 
 export default function Logo() {
   return (
     <Link href="/">
       <Image
-        width="70px"
+        width="140px" // "70px"
         src={logo}
         alt="I SEA logo"
       />
